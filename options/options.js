433148f3d7519cb1ff5b41f45f0fd61471f01b9d
Crowdbotics <team@crowdbotics.com>
2024-04-12 17:25:29 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "test-drone-express-47490",
+  url: "https://test-drone-express-47490.botics.co",
+  api: "https://test-drone-express-47490.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
